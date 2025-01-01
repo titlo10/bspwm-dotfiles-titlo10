b267ec7d33c08f0d3f5970a080cc573e202398b8
titlo10 <titlo10rebooting@gmail.com>
2025-01-01 16:59:45 +0300

    Add laptop packages

--- Builder/packages.py
+++ Builder/packages.py
@@ -7,7 +7,11 @@ BASE_PACKAGES = [
     "xfce4-power-manager", "xfce4-settings", "vlc", "picom", "alsa-plugins", "alsa-tools", "alsa-utils", "xclip", "libreoffice-fresh",
     "breeze", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd", "ttf-fira-code",
     "ttf-iosevka-nerd", "telegram-desktop", "neovim", "obs-studio", "discord",
-    "flameshot", "starship", "gtk2", "gtk3", "gtk4", "openssh", "pokemon-colorscripts-git" 
+    "flameshot", "starship", "gtk2", "gtk3", "gtk4", "openssh", "pokemon-colorscripts-git", "noto-fonts", "noto-fonts-cjk", "noto-fonts-emoji", "noto-fonts-extra" 
+]
+
+LAPTOP_PACKAGES = [
+    "brightnessctl", "blueman", "bluez", "bluez-utils", "pulseaudio-bluetooth", "xorg-xbacklight"
 ]
 
 
