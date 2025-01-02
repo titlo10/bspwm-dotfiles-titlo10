ef361cca46dc453641a9dbd0d292c8b7f7519d93
titlo10 <titlo10rebooting@gmail.com>
2025-01-02 12:50:52 +0300

    Changed packages.py

--- Builder/packages.py
+++ Builder/packages.py
@@ -4,7 +4,7 @@ BASE_PACKAGES = [
     "tumbler", "ffmpegthumbnailer", "lsd", "kitty", "bat", "feh", "chromium",
     "fish", "lxappearance", "thunar", "fastfetch", "papirus-icon-theme", "pavucontrol", "polybar", "pulseaudio",
     "pulseaudio-alsa", "rofi", "calcurse", "unrar", "zip", "unzip", "xarchiver",
-    "xfce4-power-manager", "xfce4-settings", "vlc", "picom", "alsa-plugins", "alsa-tools", "alsa-utils", "xclip", "libreoffice-fresh",
+    "xfce4-settings", "vlc", "picom", "alsa-plugins", "alsa-tools", "alsa-utils", "xclip", "libreoffice-fresh",
     "breeze", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd", "ttf-fira-code",
     "ttf-iosevka-nerd", "telegram-desktop", "neovim", "obs-studio", "discord",
     "flameshot", "starship", "gtk2", "gtk3", "gtk4", "openssh", "noto-fonts", "noto-fonts-cjk", "noto-fonts-emoji", "noto-fonts-extra" 
